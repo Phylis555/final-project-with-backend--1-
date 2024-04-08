3a8a2e19799ad6fc0ea33c690d6eb37bb2907ba2
LiatSagi <liat9171@gmail.com>
2024-04-08 15:19:18 +0300

    Fix donation creation

--- frameworks/Frontend/src/components/donator/createDonation.js
+++ frameworks/Frontend/src/components/donator/createDonation.js
@@ -5,11 +5,11 @@ import swal from "sweetalert";
 import { newDonation } from "../../api/donator.api";
 import NavBar from "../NavBar";
 import DonatorDashboard from "./donatorDashboard";
-import { requesterProfile } from '../../api/requester.api';
 
 import LoadingSpinner from "../common/LoadingSpinner";
 import { getCookie } from "../common/getCookie";
 import Footer from "../Footer";
+// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 export default function CreateDonation() {
   var dtToday = new Date();
@@ -29,6 +29,7 @@ export default function CreateDonation() {
 
   const navigate = useNavigate();
   const [donationTitle, setDonationTitle] = useState("");
+  const [email, setEmail] = useState("");
   const [contactNumber, setContactNumber] = useState("");
   const [location, setLocation] = useState("");
   const [donationDescription, setDonationDescription] = useState("");
@@ -36,27 +37,19 @@ export default function CreateDonation() {
 
   const [loading, setLoading] = useState(false);
   const [userId, setUserId] = useState("");
+  // const [category, setCategory] = useState(""); 
+
   const [wantedItems, setWantedItems] = useState([]); 
   const [itemName, setItemName] = useState('');
   const [selectedCategory, setSelectedCategory] = useState('');
   const [wantedQuantity, setWantedQuantity] = useState('');
-  const [profileData, setProfileData] = useState(null);
 
+  // const handleCategoryChange = (e) => {
+  //   setCategory(e.target.value); 
+  // };
+  
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      const userId = getCookie("uId");
-      setUserId(userId);
-      try {
-        const res = await requesterProfile(userId);
-        setProfileData(res.data.requester);
-        setLoading(false);
-      } catch (error) {
-        console.error("Error fetching user details:", error);
-        setLoading(false);
-      }
-    };
-    fetchData();
+    setUserId(getCookie("uId"));
   }, []);
 
   let filesarr = [];
@@ -92,10 +85,14 @@ export default function CreateDonation() {
     setLoading(true);
     e.preventDefault();
     const donationImage = filesarr.base64;
+    console.log(donationImage);
+    const userID = userId;
+
+
     const donation = {
-      userID: userId,
+      userID,
       donationTitle,
-      email: profileData.email,
+      email,
       location,
       donationEndDate,
       contactNumber,
@@ -104,24 +101,25 @@ export default function CreateDonation() {
       wantedItems
     };
     console.log(donation);
-    try {
-      await newDonation(donation);
-      setLoading(false);
-      swal("תרומה נוצרה בהצלחה", "", "success").then((value) => {
-        if (value) {
-          navigate("../dashboard");
-        }
-      });
-    } catch (err) {
-      console.log(err);
-      swal("יצירת התרומה נכשלה", "בבקשה נסה שוב", "error").then(
-        (value) => {
+    await newDonation(donation)
+      .then((res) => {
+        setLoading(false);
+        swal("תרומה נוצרה בהצלחה", "", "success").then((value) => {
           if (value) {
             navigate("../dashboard");
           }
-        }
-      );
-    }
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("יצירת התרומה נכשלה", "בבקשה נסה שוב", "error").then(
+          (value) => {
+            if (value) {
+              navigate("../dashboard");
+            }
+          }
+        );
+      });
   };
 
   return (
@@ -136,6 +134,7 @@ export default function CreateDonation() {
             bottom: 0,
             left: 0,
             right: 0,
+
             margin: "auto",
           }}
         >
@@ -192,7 +191,8 @@ export default function CreateDonation() {
                       />
                     </div>
                     <label className= "my-3">פרטי איש קשר:</label>
-                    <div class="input-group mb-3 input-group input-group-outline mb-3">
+
+                    <div class="input-group  input-group input-group-outline mb-3">
                       
                       <input
                         type="text"
@@ -202,111 +202,106 @@ export default function CreateDonation() {
                         title="מספר טלפון בעל 10 ספרות"
                         pattern="[0]{1}[0-9]{9}"
                         class="form-control"
-                        value={profileData ? '0'+profileData.contactNumber : ''}
                         onChange={(e) => {
                           setContactNumber(e.target.value);
                         }}
                       />
                     </div>
-                    <div class="input-group  input-group input-group-outline ">
+                    <div class="input-group input-group input-group-outline ">
                       <input
                         type="email"
                         class="form-control"
                         placeholder="Email*"
                         aria-label="Email"
                         aria-describedby="basic-addon1"
-                        value={profileData ? profileData.email : ''}
                         onChange={(e) => {
                           setEmail(e.target.value);
                         }}
                         required
                       />
                     </div>
-                      <label className= "my-3">תאריך סיום התרומה:</label>
-                      <div className="input-group input-group-outline mb-3">
-                        <input
-                          placeholder="תאריך סיום התרומה"
-                          className="form-control"
-                          type="date"
-                          min={minDate}
-                          max={maxDate}
-                          id="datefield"
-                          onFocus={(e) => (e.target.type = "date")}
-                          onChange={(e) => {
-                            setDonationEndDate(e.target.value);
-                          }}
-                          required
-                        />
-                      </div>
-                    
-                    
-                    
-                    <label className= "">הוספת פריטים:</label>
-                     <div className="input-group   input-group-outline align-items-center"> 
-                      <select
-                        className="form-select ms-2 rounded-2"
-                        aria-label="Select Category"
-                        value={selectedCategory}
-                        onChange={(e) => setSelectedCategory(e.target.value)}
-                      >
-                        <option value="">בחר קטגוריה</option>
-                        {categories.map((cat, index) => (
-                          <option key={index} value={cat}>{cat}</option>
-                        ))}
-                      </select>
+                    <label className= "my-3">תאריך סיום התרומה:</label>
+                    <div class="input-group input-group input-group-outline mb-3">
                       <input
-                        type="text"
-                        className="form-control ms-2 rounded-2"
-                        placeholder="שם הפריט"
-                        value={itemName}
-                        onChange={(e) => setItemName(e.target.value)}
-                      />
-                    
-                      <input
-                        type="number"
-                        className="form-control ms-2 rounded-2"
-                        placeholder="כמות מבוקשת"
-                        value={wantedQuantity}
-                        onChange={(e) => setWantedQuantity(e.target.value)}
-                      />
-                      <button className="btn btn-primary rounded-start me-2 mt-2" onClick={handleAddItem}>                        
-                        <i className="fas fa-plus" style={{ marginLeft: '5px' }}></i>
-                          הוסף
-                      </button>
-                      </div>
-                    <div>
-                      {wantedItems.length > 0 && (
-                        <div>
-                          <h2>הפריטים המבוקשים:</h2>
-                          <ul>
-                            {wantedItems.map((item, index) => (
-                              <li key={index}>
-                                {item.itemName} - {item.category} - {item.quantity}
-                              </li>
-                            ))}
-                          </ul>
-                        </div>
-                      )}
-                    </div>
-                    <div class="input-group mb-3 input-group input-group-outline mb-3">
-                      <textarea
+                        placeholder="תאריך סיום התרומה"
                         class="form-control"
-                        placeholder="תיאור אודות התרומה*"
-                        id="exampleFormControlTextarea1"
-                        rows="3"
+                        type="date"
+                        min={minDate}
+                        max={maxDate}
+                        id="datefield"
+                        onFocus={(e) => (e.target.type = "date")}
                         onChange={(e) => {
-                          setDonationDescription(e.target.value);
+                          setDonationEndDate(e.target.value);
                         }}
-                        required
-                      ></textarea>
+                      />
                     </div>
-                    <div>הוספת תמונה</div>
-                    <FileBase64 onDone={(files) => fileUpload(files)} />
-                    <div class="text-center">
-                      <button type="submit" class="btn btn-secondary">
-                        יצירת בקשה לתרומה
-                      </button>
+                    
+                  <label className= "">הוספת פריטים:</label>
+                   <div className="input-group input-group-outline align-items-center"> 
+                    <select
+                      className="form-select ms-2 rounded-2"
+                      aria-label="Select Category"
+                      value={selectedCategory}
+                      onChange={(e) => setSelectedCategory(e.target.value)}
+                    >
+                      <option value="">בחר קטגוריה</option>
+                      {categories.map((cat, index) => (
+                        <option key={index} value={cat}>{cat}</option>
+                      ))}
+                    </select>
+                    <input
+                      type="text"
+                      className="form-control ms-2 rounded-2"
+                      placeholder="שם הפריט"
+                      value={itemName}
+                      onChange={(e) => setItemName(e.target.value)}
+                    />
+                  
+                    <input
+                      type="number"
+                      className="form-control ms-2 rounded-2"
+                      placeholder="כמות מבוקשת"
+                      value={wantedQuantity}
+                      onChange={(e) => setWantedQuantity(e.target.value)}
+                    />
+                    <button className="btn btn-primary rounded-start me-2 mt-2" onClick={handleAddItem}>                        
+                      <i className="fas fa-plus" style={{ marginLeft: '5px' }}></i>
+                        הוסף
+                    </button>
                     </div>
+                  <div>
+                    {wantedItems.length > 0 && (
+                      <div>
+                        <h2>הפריטים המבוקשים:</h2>
+                        <ul>
+                          {wantedItems.map((item, index) => (
+                            <li key={index}>
+                              {item.itemName} - {item.category} - {item.quantity}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </div>
+                  <div class="input-group mb-3 input-group input-group-outline mb-3">
+                    <textarea
+                      class="form-control"
+                      placeholder="תיאור אודות התרומה*"
+                      id="exampleFormControlTextarea1"
+                      rows="3"
+                      onChange={(e) => {
+                        setDonationDescription(e.target.value);
+                      }}
+                      required
+                    ></textarea>
+                  </div>
+                  <div>הוספת תמונה</div>
+                  <FileBase64 onDone={(files) => fileUpload(files)} />
+                  <div class="text-center">
+                    <button type="submit" class="btn btn-secondary">
+                      יצירת בקשה לתרומה
+                    </button>
+                  </div>
                   </form>
                 </div>
               </div>
@@ -324,3 +319,9 @@ export default function CreateDonation() {
     </>
   );
 }
+
+
+              
+                 
+                  
+                  
