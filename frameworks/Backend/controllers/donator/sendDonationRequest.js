a880eaeeddee2ecbde7e13544ed0cf94443402c4
LiranMeirovich <Liranmeirovich@gmail.com>
2024-04-27 16:32:18 +0300

    fixed an oopsie

--- frameworks/Backend/controllers/donator/sendDonationRequest.js
+++ frameworks/Backend/controllers/donator/sendDonationRequest.js
@@ -1,10 +1,12 @@
 const Donation = require("../../models/donation.model");
 const DonationRequest = require("../../models/donationRequest.model");
 const Item = require("../../models/item.model");
+const { validationResult } = require('express-validator/check');
 
 const sendDonationRequest = async (req, res) => {
   try {
     const errors = validationResult(req);
+    console.log("lol");
     console.log(errors);
     if (!errors.isEmpty()) {
       return res.status(422).json({message: 'Validation failed.', error : errors.array()});
