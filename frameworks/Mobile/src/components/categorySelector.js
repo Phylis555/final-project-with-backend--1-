833e3ec576da511e2ca46a5541649b8a0dd10165
LiranMeirovich <liranmeirovich@gmail.com>
2024-06-10 12:46:04 +0300

    Removed some small things

--- frameworks/Mobile/src/components/categorySelector.js
+++ frameworks/Mobile/src/components/categorySelector.js
@@ -1,87 +1,77 @@
-import React, { useState } from "react"
-import { StyleSheet, View, Text } from "react-native"
-import {Picker} from '@react-native-picker/picker'
-import Colors from "../utils/colors"
-import { withDecay } from "react-native-reanimated"
-import { Dropdown } from 'react-native-element-dropdown';
-import { categoryData } from "../utils/Constant"
-
-
-
+import React, { useState } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+import Colors from "../utils/colors";
+import { withDecay } from "react-native-reanimated";
+import { Dropdown } from "react-native-element-dropdown";
+import { categoryData } from "../utils/Constant";
 
 const CategoryA = (props) => {
-    
-    const [selectedCategory, setSelectedCategory] =useState()
+  const [selectedCategory, setSelectedCategory] = useState();
 
-    // return (
-    //     <View>
-    //         <Text style={styles.label}>Category</Text>
-    //         <View  style={styles.pickerContainer}>
-    //             <Picker
-    //                 selectedValue={selectedCategory}
-    //                 onValueChange={(itemValue, itemIndex) =>
-    //                     setSelectedCategory(itemValue)
-    //                 }>
-    //                 <Picker.Item label="general" value="General" />
-    //                 <Picker.Item label="books" value="Books" />
-    //                 <Picker.Item label="clothing" value="Clothing" />
-    //                 <Picker.Item label="gadgets" value="Gadgets" />
-    //             </Picker>
-    //         </View>
-    //     </View>
-    // );
+  // return (
+  //     <View>
+  //         <Text style={styles.label}>Category</Text>
+  //         <View  style={styles.pickerContainer}>
+  //             <Picker
+  //                 selectedValue={selectedCategory}
+  //                 onValueChange={(itemValue, itemIndex) =>
+  //                     setSelectedCategory(itemValue)
+  //                 }>
+  //                 <Picker.Item label="general" value="General" />
+  //                 <Picker.Item label="books" value="Books" />
+  //                 <Picker.Item label="clothing" value="Clothing" />
+  //                 <Picker.Item label="gadgets" value="Gadgets" />
+  //             </Picker>
+  //         </View>
+  //     </View>
+  // );
 
-    return (
-        <Dropdown
-          style={styles.dropdown}
-          placeholderStyle={styles.placeholderStyle}
-          selectedTextStyle={styles.selectedTextStyle}
-          inputSearchStyle={styles.inputSearchStyle}
-          iconStyle={styles.iconStyle}
-          data={categoryData}
-          search={false}
-          maxHeight={300}
-          labelField="label"
-          valueField="value"
-          placeholder="בחר קטגוריה"
-          {...props}
-         
-        //   onChange={item => {
-        //     setSelectedCategory(item.value);
-        //   }}
-          
-        />
-      );
+  return (
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      inputSearchStyle={styles.inputSearchStyle}
+      iconStyle={styles.iconStyle}
+      data={categoryData}
+      search={false}
+      maxHeight={300}
+      labelField="label"
+      valueField="value"
+      placeholder="בחר קטגוריה"
+      {...props}
 
-
-
-
-}
+      //   onChange={item => {
+      //     setSelectedCategory(item.value);
+      //   }}
+    />
+  );
+};
 
 const CategoryB = () => {
-    
-    const [selectedCategory, setSelectedCategory] =useState()
+  const [selectedCategory, setSelectedCategory] = useState();
 
-    return (
-        <View>
-            <Text style={styles.label}>Category</Text>
-            <View  style={styles.pickerContainer}>
-                <Picker
-                    selectedValue={selectedCategory}
-                    onValueChange={(itemValue, itemIndex) =>
-                        setSelectedCategory(itemValue)
-                    }>
-                    <Picker.Item label="General" value="general" />
-                    <Picker.Item label="Technology" value="tech" />
-                    <Picker.Item label="Food" value="food" />
-                </Picker>
-            </View>
-        </View>
-    );
-}
+  return (
+    <View>
+      <Text style={styles.label}>Category</Text>
+      <View style={styles.pickerContainer}>
+        <Picker
+          selectedValue={selectedCategory}
+          onValueChange={(itemValue, itemIndex) =>
+            setSelectedCategory(itemValue)
+          }
+        >
+          <Picker.Item label="General" value="general" />
+          <Picker.Item label="Technology" value="tech" />
+          <Picker.Item label="Food" value="food" />
+        </Picker>
+      </View>
+    </View>
+  );
+};
 
 export { CategoryA, CategoryB };
-
 
 // const styles = StyleSheet.create({
 //     pickerContainer: {
@@ -93,43 +83,39 @@ export { CategoryA, CategoryB };
 //         marginBottom: 10
 //     },
 //     label: {
-//         fontSize: 16, 
-//         fontWeight: '500', 
-//         color: Colors.light, 
+//         fontSize: 16,
+//         fontWeight: '500',
+//         color: Colors.light,
 //         marginVertical: 10
 //     }
 // })
 
 const styles = StyleSheet.create({
-    dropdown: {
-      margin: 16,
-      height: 50,
-      textAlign: 'right',
-      flex:1,
-      borderBottomColor: Colors.primary,
-      borderBottomWidth: 0.5,
-    },
-    icon: {
-      marginRight: 5,
-    },
-    placeholderStyle: {
-      fontSize: 16,
-      textAlign: 'right'
-    },
-    selectedTextStyle: {
-      fontSize: 16,
-      textAlign: 'center'
-
-    },
-    iconStyle: {
-      width: 20,
-      height: 20,
-     
-    },
-    inputSearchStyle: {
-      height: 40,
-      fontSize: 16,
-      
-
-    },
-  });
+  dropdown: {
+    margin: 16,
+    height: 50,
+    textAlign: "right",
+    flex: 1,
+    borderBottomColor: Colors.primary,
+    borderBottomWidth: 0.5,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+    textAlign: "right",
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+    textAlign: "center",
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
